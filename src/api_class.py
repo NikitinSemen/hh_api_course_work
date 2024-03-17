4e470c5640887802e6e178b0177d0f1772ad95d7
NikitinSemen <pr.pypblrka@gmail.com>
2024-03-17 11:12:10 +0400

    в методы добавлены корекция выводимого текста

--- src/api_class.py
+++ src/api_class.py
@@ -1,13 +1,22 @@
 class Vacancy:
-    def __init__(self, name, salary, experience, roles, requirement):
+    def __init__(self, name, salary, experience, roles, requirement, url):
         self.name = name
         self.salary = salary
         self.experience = experience
         self.roles = roles
         self.requirement = requirement
+        self.url = url
+
+    def __str__(self):
+        return f'{self.name}, {self.salary}., {self.experience}, {self.roles}'
 
     def get_requirement(self):
-        return self.requirement
+        if self.requirement is None:
+            return f'Требования не указаны'
+        else:
+            new_text = self.requirement.replace('<highlighttext>', '')
+            new_text_1 = new_text.replace("</highlighttext>", '')
+            return new_text_1
 
     def get_name(self):
         return self.name
@@ -23,12 +32,13 @@ class Vacancy:
         elif self.salary['from'] is None:
             return f'Зарплата до {self.salary["to"]}.{self.salary["currency"]}'
         elif self.salary['to']:
-            return f'Зарплата от {self.salary["from"]}.{self.salary["currency"]} до {self.salary["to"]}.{self.salary["currency"]}'
+            return (f'Зарплата от {self.salary["from"]}.{self.salary["currency"]}'
+                    f' до {self.salary["to"]}.{self.salary["currency"]}')
         else:
             return f'Зарплата {self.salary["from"]}.{self.salary["currency"]}'
 
     def get_role(self):
         return self.roles
 
-    def __str__(self):
-        return f'{self.name}, {self.salary}, {self.experience}, {self.roles}'
+    def get_url(self):
+        return self.url
